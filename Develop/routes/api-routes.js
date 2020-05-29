7177e490509ec350f1d7a18ec23888c445227655
Jasmine Epps <epps.jasmine@gmail.com>
2020-05-28 22:33:50 -0400

    The stats.html file is completed.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -2,12 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   app.get("/api/workouts", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
     db.Workout.find({
       // include: [db.Post]
     }).then(function(dbWorkout) {
+      res.json(dbWorkout);
+    });
+    console.log("This api workouts route is working");
+    // res.send("working");
+  });
+
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({
+      // include: [db.Post]
+    })
+    .limit(7)
+    .then(function(dbWorkout) {
       res.json(dbWorkout);
     });
     console.log("This api workouts route is working");
